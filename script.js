17782357d931d25d72598a7d13e19a7d2a52ee05
Ilya <il.kii@mail.ru>
2021-09-19 05:47:56 +0200

    Lower contact form can send info to server

--- script.js
+++ script.js
@@ -234,6 +234,7 @@ window.addEventListener("DOMContentLoaded", function () {
   };
 
   let form = document.querySelector(".main-form"),
+    form2 = document.querySelector(".contact-you"),
     input = document.getElementsByTagName("input"),
     statusMessage = document.createElement("div");
 
@@ -254,6 +255,38 @@ window.addEventListener("DOMContentLoaded", function () {
     });
 
     let json = JSON.stringify(obj);
+    request.send(json);
+
+    request.addEventListener("readystatechange", function () {
+      if (request.readyState < 4) {
+        statusMessage.innerHTML = message.loading;
+      } else if (request.readyState === 4 && request.status == 200) {
+        statusMessage.innerHTML = message.success;
+      } else {
+        statusMessage.innerHTML = message.fail;
+      }
+    });
+    for (let i = 0; i < input.length; i++) {
+      input[i].value = "";
+    }
+  });
+
+  form2.addEventListener("submit", function (e) {
+    e.preventDefault();
+    form2.appendChild(statusMessage);
+
+    let request = new XMLHttpRequest();
+    request.open("POST", "server.php");
+    request.setRequestHeader("Content-type", "application/json; charset=utf-8");
+
+    let form2Data = new FormData(form2);
+    let obj2 = {};
+    form2Data.forEach(function (value, key) {
+      obj2[key] = value;
+    });
+    statusMessage.style.color = "white";
+    statusMessage.style.marginTop = "30px";
+    let json = JSON.stringify(obj2);
     request.send(json);
 
     request.addEventListener("readystatechange", function () {
